ba5650a2c521d24fbb0e5043cc0880a37276bce8
rigvedsarougi77 <157096420+rigvedsarougi77@users.noreply.github.com>
2024-01-20 12:21:55 +0530

    app.py

--- app.py
+++ app.py
@@ -1,41 +1,41 @@
 import streamlit as st
-import torch
 import whisper
-import pytube
-import librosa
-import IPython.display as ipd
+import time
 
-# Load the Whisper model
-model_m = whisper.load_model('medium')
+header = st.container()
+explain = st.container()
+pre = st.container()
 
-def transcribe_audio(file_path):
-    try:
-        transcription = model_m.transcribe(file_path, fp16=False)['text']
-        return transcription
-    except Exception as e:
-        return str(e)
+with header:
 
-def main():
-    st.title("Audio Transcription App")
+    st.title("Convert Speech to Text")
+    st.subheader("Note: This Model can Only Trascribe from English Speech to English Text")
+    st.write("1. Upload Audio File \n 2. Wait till Transcribe Button Appears \n 3. Click on transcribe button and wait till result generates \n 4. Can  download the text in .txt format \n 5. If you Liked it then Press the Button.")
 
-    uploaded_file = st.file_uploader("Upload an MP3 audio file", type=["mp3"])
+with explain: 
+    # Upload audio file with streamlit
+    audio_file = st.file_uploader("Upload Audio", type=["wav","mp3","m4a"])
 
-    if uploaded_file is not None:
-        # Save the uploaded file to a temporary location
-        file_path = "temp_audio_file.mp3"
-        with open(file_path, "wb") as f:
-            f.write(uploaded_file.getvalue())
+    model = whisper.load_model("medium")
 
-        st.audio(file_path, format="audio/mp3", start_time=0)
+    def get_audio_file_details(file):
+        file_details = {"FileName": file.name, "FileType": file.type, "FileSize":file.size}
+        return file_details
 
-        if st.button("Transcribe"):
-            transcription = transcribe_audio(file_path)
-            st.write("Transcription:")
-            st.write(transcription)
+    if st.sidebar.button("Transcribe Audio"):
+        if audio_file is not None:
+            st.sidebar.success("Transcribing Audio")
+            transcription = model.transcribe(audio_file.name)
+            st.sidebar.success("Transcription Complete")
+            result = transcription["text"]
+            st.markdown(result)
+            st.download_button(label = 'Download Text',data= result,file_name = "Result.txt")
+        else:
+            st.sidebar.error("Please Upload an Audio File")
 
-    st.markdown(
-        "Note: This app uses the Whisper ASR model for transcription. It may not be perfect and accuracy may vary."
-    )
+    st.sidebar.header("Play Original Audio File")
+    st.sidebar.audio(audio_file)
 
-if __name__ == "__main__":
-    main()
+with pre:
+    if st.sidebar.button("Press if you Like It"):
+        st.balloons()
